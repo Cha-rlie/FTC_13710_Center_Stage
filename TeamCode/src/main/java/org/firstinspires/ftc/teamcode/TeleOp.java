9464b00b47c5ca68d95ef4da0359778b7eec1634
Cha-rlie <c7ts2charliehome@outlook.com>
2023-10-16 22:52:03 +1000

    Switch to Mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -3,39 +3,46 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.button.Button;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.GyroEx;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.hardware.Deposit;
+import org.firstinspires.ftc.teamcode.hardware.Drivebase;
 import org.firstinspires.ftc.teamcode.hardware.Hang;
 import org.firstinspires.ftc.teamcode.hardware.Intake;
 import org.firstinspires.ftc.teamcode.hardware.Transfer;
-import org.firstinspires.ftc.teamcode.hardware.swervedrive.Robot;
-import org.firstinspires.ftc.teamcode.hardware.swervedrive.Vector2d;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Diff Swerve TeleOp", group = "TeleOp")
 
 public class TeleOp extends OpMode {
-    Robot robot;
+    Drivebase drivebase;
+    IMU imuGyro;
     GamepadEx driveOp;
     GamepadEx toolOp;
 
-    // Declaring Commands
-    //deadband for joysticks
-    public double DEADBAND_MAG = 0.1;
-    public Vector2d DEADBAND_VEC = new Vector2d(DEADBAND_MAG, DEADBAND_MAG);
-
-
     public boolean willResetIMU = true;
 
+    // Declaring Commands
     private Deposit deposit;
     private Intake intake;
     private Hang hang;
 
     public void init() {
-        robot = new Robot(this, false);
+        drivebase = new Drivebase(hardwareMap);
+        // Initialise the IMU with the correct orientation
+        imuGyro.initialize(new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                )
+        ));
         driveOp = new GamepadEx(gamepad1);
         toolOp = new GamepadEx(gamepad2);
         deposit = new Deposit(hardwareMap);
@@ -58,7 +65,7 @@ public class TeleOp extends OpMode {
     }
 
     public void start () {
-        if (willResetIMU) robot.initIMU();
+        if (willResetIMU) imuGyro.resetYaw();
     }
 
 
@@ -67,33 +74,15 @@ public class TeleOp extends OpMode {
         // This must be called from the function that loops in the opmode for everything to run
         CommandScheduler.getInstance().run();
 
+        // Run the drivebase with the driveOp gamepad
+        drivebase.userControlledDrive(driveOp, imuGyro.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+
         // Update the variables
         if (driveOp.isDown(GamepadKeys.Button.A)) {
             intake.spin();
         } else if (!driveOp.isDown(GamepadKeys.Button.A)) {
             intake.spinFor5Seconds();
         }
-
-        Vector2d joystick1 = new Vector2d(gamepad1.left_stick_x, -gamepad1.left_stick_y); //LEFT joystick
-        Vector2d joystick2 = new Vector2d(gamepad1.right_stick_x, -gamepad2.right_stick_y); //RIGHT joystick
-
-        robot.driveController.updateUsingJoysticks(checkDeadband(joystick1), checkDeadband(joystick2));
-
-//        //uncomment for live tuning of ROT_ADVANTAGE constant
-//        if (gamepad1.b) {
-//            robot.driveController.moduleRight.ROT_ADVANTAGE += 0.01;
-//            robot.driveController.moduleLeft.ROT_ADVANTAGE += 0.01;
-//        }
-//        if (gamepad1.x) {
-//            robot.driveController.moduleRight.ROT_ADVANTAGE -= 0.01;
-//            robot.driveController.moduleLeft.ROT_ADVANTAGE -= 0.01;
-//        }
-//        telemetry.addData("ROT_ADVANTAGE: ", robot.driveController.moduleLeft.ROT_ADVANTAGE);
-
-
-        //to confirm that joysticks are operating properly
-        telemetry.addData("Joystick 1", joystick1);
-        telemetry.addData("Joystick 2", joystick2);
 
         Button transferButton = new GamepadButton(
                 toolOp, GamepadKeys.Button.B
@@ -129,13 +118,5 @@ public class TeleOp extends OpMode {
         telemetry.addData("Slide Pos 1: ", deposit.DS1.motor.getCurrentPosition());
         telemetry.addData("Slide Pos 2: ", deposit.DS2.motor.getCurrentPosition());
         telemetry.update();
-    }
-
-    //returns zero vector if joystick is within deadband
-    public Vector2d checkDeadband(Vector2d joystick) {
-        if (Math.abs(joystick.getX()) > DEADBAND_VEC.getX() || Math.abs(joystick.getY()) > DEADBAND_VEC.getY()) {
-            return joystick;
-        }
-        return Vector2d.ZERO;
     }
 }
