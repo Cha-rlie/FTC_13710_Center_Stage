8883a98df3b0786d6db5e82cbb3b35648c4c22e8
Cha-rlie <c7ts2charliehome@outlook.com>
2023-11-04 08:08:31 +1000

    Add Team Prop Detection Capabilities

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/propLocationDetection/propLocationDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/propLocationDetection/propLocationDetector.java
@@ -12,6 +12,9 @@ public class propLocationDetector {
     // Variables for vision processing
     private TfodProcessor tfod;
     private VisionPortal visionPortal;
+    private static String TFOD_MODEL_FILE;
+
+    public static final String[] LABELS = {"Prop"};
 
     // Variables for state management
     private String[] sides = new String[]{"LEFT","FRONT","RIGHT"};
@@ -22,8 +25,16 @@ public class propLocationDetector {
     private int[] frontDetectionClip = new int[]{300, 0, 300, 0};
     private int[] rightDetectionClip = new int[]{500, 0, 0, 0};
 
-    private void initDetector(WebcamName webcam) {
-        tfod = TfodProcessor.easyCreateWithDefaults();
+    private void initDetector(WebcamName webcam, String alianceColor) {
+        if (alianceColor.equals("BLUE")) {
+            TFOD_MODEL_FILE = "models/Blue_Team_Prop_Detector_File.tflite";
+        } else {
+            TFOD_MODEL_FILE = "models/Red_Team_Prop_Detector_File.tflite";
+        }
+        tfod = new TfodProcessor.Builder()
+                .setModelFileName(TFOD_MODEL_FILE)
+                .setModelLabels(LABELS)
+                .build();
         // Only consider a prop detected if it is 75% confident or more
         tfod.setMinResultConfidence((float) 0.75);
 
@@ -31,12 +42,12 @@ public class propLocationDetector {
     }
 
     private String detectPropLocation() {
-        visionPortal.resumeStreaming();
+        //visionPortal.resumeStreaming();
         while (currentlyDetecting) {
             for (String side : sides) {
                 // Selectively Obscure Some Pixels (Essentially Cropping without Changing Size)
-                if (side == "LEFT") {tfod.setClippingMargins(0, 0, 500, 0);}
-                else if (side == "FRONT") {tfod.setClippingMargins(300, 0, 300, 0);}
+                if (side.equals("LEFT")) {tfod.setClippingMargins(0, 0, 500, 0);}
+                else if (side.equals("FRONT")) {tfod.setClippingMargins(300, 0, 300, 0);}
                 else {tfod.setClippingMargins(500, 0, 0, 0);}
                 List<Recognition> currentRecognitions = tfod.getRecognitions();
 
